5a19e3859860cf1b92285253df6d735c312dfaec
SzymonDanza <szymonzlocki2001@gmail.com>
2025-01-05 17:09:40 +0100

    first commit

---
+++ kursywalut/src/main/java/com/kursywalut/kursywalut/KursywalutApplication.java
@@ -0,0 +1,13 @@
+package com.kursywalut.kursywalut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursywalutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursywalutApplication.class, args);
+	}
+
+}
---
+++ kursywalut/src/test/java/com/kursywalut/kursywalut/KursywalutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kursywalut.kursywalut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursywalutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
